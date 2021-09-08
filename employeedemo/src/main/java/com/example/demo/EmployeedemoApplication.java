739a3fbc6024823b73bb834e86d73f9170868ee3
ajithanalluri <44363166+ajithanalluri@users.noreply.github.com>
2021-09-08 12:53:21 +0530

    Update EmployeedemoApplication.java

--- employeedemo/src/main/java/com/example/demo/EmployeedemoApplication.java
+++ employeedemo/src/main/java/com/example/demo/EmployeedemoApplication.java
@@ -1,20 +1,17 @@
 package com.example.demo;
 
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class EmployeedemoApplication {
 
-	   @Value("${message}")
-	   static String message;
+// 	   @Value("${message}")
+// 	   static String message;
 	   
 	public static void main(String[] args) {
 		
-		System.out.println(message);
 		SpringApplication.run(EmployeedemoApplication.class, args);
-		System.out.println(message);
 	}
 
 }
