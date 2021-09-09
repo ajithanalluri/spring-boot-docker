8ac5b8cfb64449dd043d75755be3ac063d90a6e1
ajithanalluri <44363166+ajithanalluri@users.noreply.github.com>
2021-09-09 17:47:11 +0530

    added host check

--- appenginejava11/employeedemo/src/main/java/com/example/demo/EmployeedemoApplication.java
+++ appenginejava11/employeedemo/src/main/java/com/example/demo/EmployeedemoApplication.java
@@ -1,9 +1,14 @@
 package com.example.demo;
 
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class EmployeedemoApplication {
 
 //	   @Value("${message}")
@@ -12,6 +17,14 @@ public class EmployeedemoApplication {
 	public static void main(String[] args) {
 		
 		SpringApplication.run(EmployeedemoApplication.class, args);
+	}
+	@GetMapping("/getip")
+	public String getIp() throws UnknownHostException
+	{
+		InetAddress ip = InetAddress.getLocalHost();
+		//log.info(" From host: " + ip);
+		System.out.println(" From host: " + ip);
+	        return " From host: " + ip;
 	}
 
 }
